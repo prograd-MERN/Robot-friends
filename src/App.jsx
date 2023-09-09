5bbbcee5c3d9994d6cc00d8e0e2215faf0fcdbeb
Balaramansethu <baalaaraman@gmail.com>
2023-09-10 02:42:18 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CardComponents from './Components/CardComponents/CardComponents'
+
+function App() {
+  return (
+<React.Fragment>
+ <div>HELLO</div>
+ <CardComponents/>
+</React.Fragment>  )
+}
+export default App
---
+++ src/Components/CardComponents/CardComponents.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardComponents = (props) => {
+  return (
+<React.Fragment>
+    <div>THERE</div>
+</React.Fragment>
+  )
+}
+
+export default CardComponents
